69d18f3e303ca3db7ea683e7c24b196ea81f352b
TimKnipe <timk1971@gmail.com>
2020-04-25 14:37:03 +1000

    convert class to fn

--- FrameComponent.tsx
+++ FrameComponent.tsx
@@ -3,15 +3,10 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme();
 
-// Playroom 0.16.1 needs this to be a class component
-export default class FrameComponent extends React.Component {
-  componentDidMount() {
+const FrameComponent: FunctionComponent<{}> = ({ children }) => {
+  useEffect(() => {
     document.getElementsByTagName('body')[0].style.margin = '0';
-  }
-
-  render() {
-    const { children } = this.props;
-
-    return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
-  }
-}
+  }, []);
+  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+};
+export default FrameComponent;
