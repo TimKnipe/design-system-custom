4ca66cd010a0cfcdfb67dd1acb489773449eebd7
Tim Knipe <tknipe@officeworks.com.au>
2020-05-12 20:02:23 +1000

    .

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,8 +1,17 @@
 import React, { FunctionComponent } from 'react';
 import { Typography, TypographyProps } from '@material-ui/core';
+import basekick from './utils/basekick';
 
 export interface TextProps extends TypographyProps {}
 
-export const Text: FunctionComponent<TextProps> = ({ children, ...props }) => (
-  <Typography {...props}>{children}</Typography>
-);
+export const Text: FunctionComponent<TextProps> = ({ children, ...props }) => {
+  const { base, baseline, cropFirstLine } = basekick({
+    baseFontSize: 1,
+    capHeight: 1,
+    descenderHeightScale: 1,
+    gridRowHeight: 1,
+    typeRowSpan: 1,
+    typeSizeModifier: 1,
+  });
+  return <Typography {...props}>{children}</Typography>;
+};
---
+++ src/components/utils/basekick.ts
@@ -0,0 +1,55 @@
+// Based on Braid design system from Seek
+// https://github.com/seek-oss/braid-design-system/blob/dc052d40889d6f584971480b21bb6596ff4ec5b1/lib/hooks/typography/basekick.ts
+
+interface BaseKickOptions {
+  typeSizeModifier: number;
+  baseFontSize: number;
+  descenderHeightScale: number;
+  capHeight: number;
+  typeRowSpan: number;
+  gridRowHeight: number;
+}
+
+export default ({
+  typeSizeModifier,
+  baseFontSize,
+  descenderHeightScale,
+  typeRowSpan,
+  gridRowHeight,
+  capHeight,
+}: BaseKickOptions) => {
+  const fontSize = typeSizeModifier * baseFontSize;
+
+  const calculateTypeOffset = (lh: number) => {
+    const lineHeightScale = lh / fontSize;
+    return (lineHeightScale - 1) / 2 + descenderHeightScale;
+  };
+
+  const lineHeight = typeRowSpan * gridRowHeight;
+  const typeOffset = calculateTypeOffset(lineHeight);
+
+  const topSpace = lineHeight - capHeight * fontSize;
+  const heightCorrection =
+    topSpace > gridRowHeight ? topSpace - (topSpace % gridRowHeight) : 0;
+
+  const preventCollapse = 1;
+
+  return {
+    base: {
+      fontSize: `${fontSize}px`,
+      lineHeight: `${lineHeight}px`,
+    },
+    baseline: {
+      transform: `translateY(${typeOffset}em)`,
+    },
+    cropFirstLine: {
+      paddingTop: preventCollapse,
+      ':before': {
+        content: "''",
+        marginTop: -(heightCorrection + preventCollapse),
+        display: 'block',
+        height: 0,
+      },
+    },
+  };
+};
